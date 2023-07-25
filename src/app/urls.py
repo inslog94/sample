bf8d54da415f27b2a68203bd0afd63973284393e
inslog94 <131739338+inslog94@users.noreply.github.com>
2023-07-25 17:38:50 +0900

    수정

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from user.views import Login
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='user/user_login.html'), name='index'),
+    path('', Login.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('user/', include('user.urls')),
